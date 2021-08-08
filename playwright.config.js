6badfd8e57fe2ea8f24b8414eef153725983de68
rasrasa <88403581+rasrasa@users.noreply.github.com>
2021-08-08 14:23:29 +0300

    Add files via upload

---
+++ playwright.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+    use: {
+      headless: false,
+      viewport: { width: 1280, height: 720 },
+      screenshot: 'on',
+      launchOptions: { slowMo: 1000, },
+    },
+  };
+  
+  module.exports = config;
